1188e15ac585262b63fd3592e196d258c6af95da
Shunkm <okuratesu@gmail.com>
2025-11-11 12:32:42 +0900

    sidebar

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from shiny import App,render,ui
 
 app_ui = ui.page_sidebar(
     ui.sidebar(
-        ui.input_numeric("numeric","Numeric input",1,min=0,max=100),
-        ui.input
+        ui.input_numeric("numeric","治安",1,min=0,max=100),
+        ui.input_numeric("numeric","衛生",1,min=0,max=100),
+        ui.input_numeric("numeric","混雑",1,min=0,max=100),
+        ui.input_numeric("numeric","評価",1,min=0,max=100),
+        ui.input_selectize("slectize","地域を選択してください。",{"europe":"ヨーロッパ","asia":"アジア・オセアニア","america":"アメリカ","africa":"中東・アフリカ"})
 
         )
     )
