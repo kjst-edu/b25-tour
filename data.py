1696f6f605926a2b8c2bcf649251b43f61d86858
Shunkm <okuratesu@gmail.com>
2025-11-25 11:49:00 +0900

    weight_score

--- data.py
+++ data.py
@@ -4,3 +4,6 @@ import pandas as pd
 
 app_dir = Path(__file__).parent
 df = pd.read_csv(app_dir/"demo.csv")
+
+for col in ['a', 'b', 'c', 'd']:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from shiny import App,render,ui
 
 app_ui = ui.page_sidebar(
     ui.sidebar(
-        ui.input_numeric("numeric","治安",1,min=0,max=100),
-        ui.input_numeric("numeric","衛生",1,min=0,max=100),
-        ui.input_numeric("numeric","混雑",1,min=0,max=100),
-        ui.input_numeric("numeric","評価",1,min=0,max=100),
+        ui.input_numeric("a_weight","治安",1,min=0,max=100),
+        ui.input_numeric("b_weight","衛生",1,min=0,max=100),
+        ui.input_numeric("c_weight","混雑",1,min=0,max=100),
+        ui.input_numeric("d_weight","評価",1,min=0,max=100),
         ui.input_selectize("selectize","地域を選択してください。",{"europe":"ヨーロッパ","asia":"アジア・オセアニア","america":"アメリカ","africa":"中東・アフリカ"},
                            multiple=True,),
 
@@ -28,10 +28,20 @@ def server(input,output,session):
 
         # ★ 地域が選ばれていればフィルタ、選ばれていなければ全部
         if selected:
-            filtered = df[df["region"].isin(selected)]
+            filtered = df[df["region"].isin(selected)].copy()
         else:
-            filtered = df
-        return render.DataTable(filtered)
+            filtered = df.copy()
+        
+        filtered["weighted_score"] = (input.a_weight() * filtered["a"] +
+                                      input.b_weight() * filtered["b"] +
+                                      input.c_weight() * filtered["c"] +
+                                      input.d_weight() * filtered["d"] 
+                                      )
+        
+        filtered = filtered.sort_values(by="weighted_score", ascending=False)
+
+        ranking_df = filtered[["country","region","weighted_score","a","b","c","d"]].round(2)
+        return render.DataTable(ranking_df)
 
 app=App(app_ui,server)
 #%%
