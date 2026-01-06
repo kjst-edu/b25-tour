a929821a8873c4c7f11dee3f5fe68e9035528630
Shunkm <okuratesu@gmail.com>
2026-01-06 11:00:01 +0900

    not demo化

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 app_dir = Path(__file__).parent
-df = pd.read_csv(app_dir/"demo.csv")
+df = pd.read_csv(app_dir/"not demo.csv")
 
-for col in ['a', 'b', 'c', 'd']:
+for col in ["治安指数","衛生指数","国際評価指数","気候指数"]:
     df[col] = pd.to_numeric(df[col], errors='coerce')
--- main.py
+++ main.py
@@ -9,12 +9,12 @@ app_ui = ui.page_sidebar(
     ui.sidebar(
         ui.input_numeric("a_weight","治安",25,min=0,max=100),
         ui.input_numeric("b_weight","衛生",25,min=0,max=100),
-        ui.input_numeric("c_weight","混雑",25,min=0,max=100),
-        ui.input_numeric("d_weight","評価",25,min=0,max=100),
+        ui.input_numeric("c_weight","国際評価",25,min=0,max=100),
+        ui.input_numeric("d_weight","気候",25,min=0,max=100),
 
         ui.output_ui("warning_message"),
     
-        ui.input_selectize("selectize","地域を選択してください。",{"europe":"ヨーロッパ","asia":"アジア・オセアニア","america":"アメリカ","africa":"中東・アフリカ"},
+        ui.input_selectize("selectize","地域を選択してください。",{"ヨーロッパ":"ヨーロッパ","アジア・オセアニア":"アジア・オセアニア","アメリカ":"アメリカ","中東・アフリカ":"中東・アフリカ"},
                            multiple=True,),
     ),
     ui.output_data_frame("ranking_df"),
@@ -49,21 +49,21 @@ def server(input,output,session):
 
         # 地域が選ばれていればフィルタ、選ばれていなければ全部
         if selected:
-            filtered = df[df["region"].isin(selected)].copy()
+            filtered = df[df["地域"].isin(selected)].copy()
         else:
             filtered = df.copy()
         
         # 4項目のinputを利用し，加重平均を取る
-        filtered["weighted_score"] = (0.01 * input.a_weight() * filtered["a"] +
-                                      0.01 * input.b_weight() * filtered["b"] +
-                                      0.01 * input.c_weight() * filtered["c"] +
-                                      0.01 * input.d_weight() * filtered["d"] 
+        filtered["weighted_score"] = (0.01 * input.a_weight() * filtered["治安指数"] +
+                                      0.01 * input.b_weight() * filtered["衛生指数"] +
+                                      0.01 * input.c_weight() * filtered["国際評価指数"] +
+                                      0.01 * input.d_weight() * filtered["気候指数"] 
                                       )
         
         filtered = filtered.sort_values(by="weighted_score", ascending=False)
 
         # ranking_dfの定義し直し
-        ranking_df = filtered[["country","region","weighted_score","a","b","c","d"]].round(2)
+        ranking_df = filtered[["国名","地域","weighted_score","治安指数","衛生指数","国際評価指数","気候指数"]].round(2)
         return render.DataTable(ranking_df)
 
 app=App(app_ui,server)
